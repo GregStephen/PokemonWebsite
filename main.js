5167c2967f9f18299bd0d80036f7e3cc87e3a095
Greg <GregAStephen@gmail.com>
2018-11-06 20:59:36 -0600

    added some pokemon, got the type selector to work

--- main.js
+++ main.js
@@ -2,65 +2,206 @@ let pokemonArray = [
 	{
 		"name": "Bulbasaur",
 		"number": "001",
-		"type": ["Grass", "Poison"],
+		"type": ["grass", "poison"],
 		"image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
 	},{
 		"name": "Ivysaur",
 		"number": "002",
-		"type": ["Grass", "Poison"],
+		"type": ["grass", "poison"],
 		"image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png"
 	},{
 		"name": "Venusaur",
 		"number": "003",
-		"type": ["Grass", "Poison"],
+		"type": ["grass", "poison"],
 		"image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png"
 	},{
 		"name": "Charmander",
 		"number": "004",
-		"type": ["Fire"],
+		"type": ["fire"],
 		"image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png"
 	},{
 		"name": "Charmeleon",
 		"number": "005",
-		"type": ["Fire"],
+		"type": ["fire"],
 		"image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png"
 	},{
 		"name": "Charizard",
 		"number": "006",
-		"type": ["Fire", "Flying"],
+		"type": ["fire", "flying"],
 		"image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png"
 	},{
 		"name": "Squirtle",
 		"number": "007",
-		"type": ["Water"],
+		"type": ["water"],
 		"image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png"
 	},{
 		"name": "Wartortle",
 		"number": "008",
-		"type": ["Water"],
+		"type": ["water"],
 		"image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png"
 	},{
 		"name": "Blastiose",
 		"number": "009",
-		"type": ["Water"],
+		"type": ["water"],
 		"image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png"
 	},{
 		"name": "Caterpie",
 		"number": "010",
-		"type": ["Bug"],
+		"type": ["bug"],
 		"image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/010.png"
 	},{
 		"name": "Metapod",
 		"number": "011",
-		"type": ["Bug"],
+		"type": ["bug"],
 		"image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/011.png"
 	},{
 		"name": "Butterfree",
 		"number": "012",
-		"type": ["Bug", "Flying"],
+		"type": ["bug", "flying"],
 		"image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png"
 	},
+	{
+		"name": "Weedle",
+		"number": "013",
+		"type": ["bug", "poison"],
+		"image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/013.png"
+	},{
+		"name": "Kakuna",
+		"number": "014",
+		"type": ["bug", "poison"],
+		"image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/014.png"
+	},{
+		"name": "Beedrill",
+		"number": "015",
+		"type": ["bug", "poison"],
+		"image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/015.png"
+	},{
+		"name": "Pidgey",
+		"number": "016",
+		"type": ["normal", "flying"],
+		"image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
+	},{
+		"name": "Pidgeotto",
+		"number": "017",
+		"type": ["normal", "flying"],
+		"image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/017.png"
+	},{
+		"name": "Pidgeot",
+		"number": "018" ,
+		"type": ["normal", "flying"],
+		"image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/018.png"
+	},{
+		"name": "Rattata",
+		"number": "019",
+		"type": ["normal"],
+		"image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/019.png"
+	},{
+		"name": "Raticate",
+		"number": "020" ,
+		"type": ["normal"],
+		"image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/020.png"
+	},
 	// {
+	// 	"name": "",
+	// 	"number": ,
+	// 	"type": [],
+	// 	"image": 
+	// },{
+	// 	"name": "",
+	// 	"number": ,
+	// 	"type": [],
+	// 	"image": 
+	// },{
+	// 	"name": "",
+	// 	"number": ,
+	// 	"type": [],
+	// 	"image": 
+	// },{
+	// 	"name": "",
+	// 	"number": ,
+	// 	"type": [],
+	// 	"image": 
+	// },{
+	// 	"name": "",
+	// 	"number": ,
+	// 	"type": [],
+	// 	"image": 
+	// },{
+	// 	"name": "",
+	// 	"number": ,
+	// 	"type": [],
+	// 	"image": 
+	// },{
+	// 	"name": "",
+	// 	"number": ,
+	// 	"type": [],
+	// 	"image": 
+	// },{
+	// 	"name": "",
+	// 	"number": ,
+	// 	"type": [],
+	// 	"image": 
+	// },{
+	// 	"name": "",
+	// 	"number": ,
+	// 	"type": [],
+	// 	"image": 
+	// },{
+	// 	"name": "",
+	// 	"number": ,
+	// 	"type": [],
+	// 	"image": 
+	// },{
+	// 	"name": "",
+	// 	"number": ,
+	// 	"type": [],
+	// 	"image": 
+	// },{
+	// 	"name": "",
+	// 	"number": ,
+	// 	"type": [],
+	// 	"image": 
+	// },{
+	// 	"name": "",
+	// 	"number": ,
+	// 	"type": [],
+	// 	"image": 
+	// },{
+	// 	"name": "",
+	// 	"number": ,
+	// 	"type": [],
+	// 	"image": 
+	// },{
+	// 	"name": "",
+	// 	"number": ,
+	// 	"type": [],
+	// 	"image": 
+	// },{
+	// 	"name": "",
+	// 	"number": ,
+	// 	"type": [],
+	// 	"image": 
+	// },{
+	// 	"name": "",
+	// 	"number": ,
+	// 	"type": [],
+	// 	"image": 
+	// },{
+	// 	"name": "",
+	// 	"number": ,
+	// 	"type": [],
+	// 	"image": 
+	// },{
+	// 	"name": "",
+	// 	"number": ,
+	// 	"type": [],
+	// 	"image": 
+	// },{
+	// 	"name": "",
+	// 	"number": ,
+	// 	"type": [],
+	// 	"image": 
+	// },{
 	// 	"name": "",
 	// 	"number": ,
 	// 	"type": [],
@@ -91,24 +232,32 @@ let pokemonArray = [
 let searchBar = $("#searchBar");
 let searchBtn = $("#searchBtn");
 let divOutput = $("#output-places");
-
+;
 
 function showPokemon(pokemonArray) {
 	let searchVal = searchBar.val().toLowerCase();
-	console.log(searchVal);
+	let typeSelected = $("#type-select").val();
 	divOutput.html("");
+
 	for(let i of pokemonArray) {
 		console.log(i.name);
-		if (i.name.toLowerCase().includes(searchVal)) {
-			divOutput.append(`<li class = "pokemonSheet col-4"><figure><a href="#"><img src="${i.image}"></a></figure>
-				<div class = "pokemonInfo><p class="id"><span class = "number">#</span>${i.number}</p><h5>${i.name}</h5>`).children(':last').hide().fadeIn(1500);
+		console.log(i.type);
+		console.log(searchVal);
+		console.log(typeSelected);
+
+		if (i.type.includes(typeSelected)){
+			if (i.name.toLowerCase().includes(searchVal)) {
+				divOutput.append(`<li class = "pokemonSheet col-4 ${i.type}"><figure><a href="#"><img src="${i.image}"></a></figure>
+					<div class = "pokemonInfo><p class="id"><span class = "number">#</span>${i.number}</p>
+					<p class = "type">${i.type}</p><h5>${i.name}</h5>`).children(':last').hide().fadeIn(1500);
 			// (`<div class="card bg-dark text-white col" 
 			// 	style="width:18rem;"><h5 class="card-header">${i.name} #${i.number}</h5>
 			// 	<img class="card-img-top location-image" src="${i.image}"alt="${i.name}">
 			// 	<div class="card-footer">${i.type}</div></div>`).children(':last').hide().fadeIn(1500);
+			}
 		}
 	}
-}
+};
 
 $(pokemonSearchForm).submit(function(event){
 	event.preventDefault();
